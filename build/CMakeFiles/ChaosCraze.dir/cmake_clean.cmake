4c806d2ac3176502e8dff8833a2ad33c11013210
harcolemas@gmail.com <harcolemas@gmail.com>
2024-03-24 10:24:15 +0500

    Init Commit

---
+++ build/CMakeFiles/ChaosCraze.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ChaosCraze.dir/main.cpp.o"
+  "CMakeFiles/ChaosCraze.dir/main.cpp.o.d"
+  "ChaosCraze"
+  "ChaosCraze.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChaosCraze.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <raylib.h>
+using namespace std;
+
+const int height = 600;
+const int width = 800;
+
+class NPC {
+  string texturePath;
+  Texture2D npcTexture;
+  Rectangle npcRectangle;
+  Vector2 npcPosition;
+  float npcSpeed;
+
+public:
+  NPC(string texPath, Rectangle rec, Vector2 pos, float spd) {
+    npcRectangle = rec;
+    npcPosition = pos;
+    npcSpeed = spd;
+    npcTexture = LoadTexture(texPath.c_str());
+    texturePath = texPath;
+  }
+  void Draw() { DrawTextureV(npcTexture, npcPosition, WHITE); }
+};
+
+int main(int argc, char *argv[]) {
+  InitWindow(width, height, "raylib [models] example - npc Test");
+  NPC Man = NPC("../assets/Villagers/3_Man/Man.png", {0, 0, 32, 32}, {0, 0}, 2.0);
+  SetExitKey(0);
+  SetTargetFPS(60);
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    Man.Draw();
+    ClearBackground(RAYWHITE);
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
+}
