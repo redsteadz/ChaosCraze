cbe022c230d1914b25d8943ee9982b176ff97344
harcolemas@gmail.com <harcolemas@gmail.com>
2024-04-21 23:32:00 +0500

    Edited Map

--- build/CMakeFiles/ChaosCraze.dir/main.cpp.o.d
+++ build/CMakeFiles/ChaosCraze.dir/main.cpp.o.d
@@ -1,5 +1,6 @@
 CMakeFiles/ChaosCraze.dir/main.cpp.o: \
  /home/red/Documents/ProjectsDit/ChaosCraze/main.cpp \
- /usr/include/stdc-predef.h /usr/include/raylib.h \
+ /usr/include/stdc-predef.h \
+ /home/red/Documents/ProjectsDit/ChaosCraze/raylib.h \
  /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/stdarg.h \
  /usr/include/raylib-tileson.h /usr/include/raylib.h
--- main.cpp
+++ main.cpp
@@ -1,235 +1,86 @@
-#include "math.h"
-#include <iostream>
-#include <raylib.h>
-#include <string>
-#include <vector>
-using namespace std;
-const int width = 800;
-const int height = 600;
-enum STATE { attack, walk, idle, hurt, death };
+#include "raylib.h"
 
-class NPC_Characteristics {
-  STATE state;
-  int sentiment;
-  int age;
-  string occupation;
-  string name;
+#define RAYLIB_TILESON_IMPLEMENTATION
+#include "raylib-tileson.h"
 
-public:
-  NPC_Characteristics(STATE state, int sentiment, int age, string occupation,
-                      string name) {
-    this->state = idle;
-    this->sentiment = 0;
-    this->age = age;
-    this->occupation = occupation;
-    this->name = name;
-  }
-  friend class NPC;
-  virtual void setState(STATE s) { this->state = s; }
-  int getSentiment() { return sentiment; }
-  int getAge() { return age; }
-  string getOccupation() { return occupation; }
-  string getName() { return name; }
-  STATE getState() { return state; }
-  void setName(string name) { this->name = name; }
-  void setAge(int age) { this->age = age; }
-  void setOccupation(string occupation) { this->occupation = occupation; }
-  void setSentiment(int sentiment) { this->sentiment = sentiment; }
-};
+int main(int argc, char *argv[]) {
+  // Initialization
+  //--------------------------------------------------------------------------------------
+  const int screenWidth = 800;
+  const int screenHeight = 800;
+  Vector2 position = {0, 0};
 
-class NPC_Physics {
-  Vector2 npcPosition;
-  bool colliding = false;
-  float direction = 0.0f;
-  Rectangle npcRectangle;
-  int flip = 0;
-  float npcSpeed;
-
-public:
-  void Random_walk() {
-    direction +=
-        (rand() % 100 - 50) * 0.01f; // Adjust the range and step size as needed
-
-    // Calculate movement vector based on direction
-    Vector2 movement = {npcSpeed * cos(direction), npcSpeed * sin(direction)};
-
-    // Update NPC position
-    npcPosition.x += movement.x;
-    npcPosition.y += movement.y;
-    if (movement.x < 0.0f) {
-      flip = 1;
-    } else {
-      flip = 0;
-    }
-    // Ensure NPC stays within the screen boundaries
-    npcPosition.x = fmax(fmin(npcPosition.x, width - npcRectangle.width), 0);
-    npcPosition.y = fmax(fmin(npcPosition.y, height - npcRectangle.height), 0);
-  }
-  friend class NPC;
-};
-
-class NPC : public NPC_Physics, NPC_Characteristics {
-
-  float timer = 0.0f;
-  int frame = 0;
-  float frameWidth = 0;
-
-  bool isMoving = false;
-
-  string texturePath;
-  Texture2D npcTexture;
-  int frameSpeed = 6;
-  int frameCount = 0;
-  int currentFrame = 0;
-
-public:
-  NPC(string _name, Vector2 pos, float spd, STATE state, int sentiment, int age,
-      string occupation)
-      : NPC_Characteristics(state, sentiment, age, occupation, _name) {
-    npcSpeed = spd;
-    texturePath = "../assets/" + occupation + "/" + _name + "/" + _name;
-    setState(state);
-    npcPosition = pos;
-    npcRectangle = {0, 0, (float)npcTexture.height, (float)npcTexture.height};
+  // Make sure we're running in the correct directory.
+  const char *dir = GetDirectoryPath(argv[0]);
+  if (!ChangeDirectory(dir)) {
+    TraceLog(LOG_ERROR, "Unable to change directory to: %s", dir);
+    return 1;
   }
 
-  void setState(STATE s) {
-    state = s;
-    UnloadTexture(npcTexture);
-    switch (s) {
-    case walk:
-      npcTexture = LoadTexture((texturePath + "_walk.png").c_str());
-      break;
-    case idle:
-      npcTexture = LoadTexture((texturePath + "_idle.png").c_str());
-      break;
-    // One time
-    case attack:
-      npcTexture = LoadTexture((texturePath + "_attack.png").c_str());
-      break;
-    case hurt:
-      npcTexture = LoadTexture((texturePath + "_hurt.png").c_str());
-      break;
-    case death:
-      cout << " I CALL ONTO THE DEATH REAPER " << endl;
-      npcTexture = LoadTexture((texturePath + "_death.png").c_str());
-      break;
-    default:
-      break;
-    }
-    if (state == walk)
-      isMoving = true;
-    else
-      isMoving = false;
-    npcRectangle = {0, 0, (float)npcTexture.height, (float)npcTexture.height};
-    frame = 0;
-  }
-  void Draw() {
-    if (flip) {
-      DrawTextureRec(npcTexture, flippedTexture(), npcPosition, WHITE);
-    } else {
-      DrawTextureRec(npcTexture, npcRectangle, npcPosition, WHITE);
-    }
-  }
-  void Update() {
-    Animate();
-    if (isMoving)
-      Random_walk();
-    Draw();
-  }
-  void Animate() {
-    int count = 0;
-    int numTextures = 0;
-    if (npcTexture.width > 0 && npcTexture.height > 0) {
-      numTextures = npcTexture.width / npcTexture.height;
-      if (numTextures > 0) {
-        frameWidth = static_cast<float>(npcTexture.width / numTextures);
-      }
-    }
-    int maxFrames = static_cast<int>(npcTexture.width / frameWidth);
-    timer += GetFrameTime();
-    if (timer >= 0.3f) {
-      timer = 0.0f;
-      frame += 1;
-      if (state == walk || state == idle)
-        frame = frame % maxFrames;
-      else {
-        if (frame >= maxFrames)
-          frame = maxFrames - 1;
-      }
-    }
+  InitWindow(screenWidth, screenHeight, "[raylib-tileson] example");
 
-    cout << frame << endl;
-    npcRectangle = {frameWidth * frame, 0, frameWidth,
-                    static_cast<float>(npcTexture.height)};
-  }
-
-private:
-  Rectangle flippedTexture() {
-    Rectangle npcFlipped = npcRectangle;
-    npcFlipped.x =
-        npcRectangle.x + npcRectangle.width; // Set x-coordinate to right edge
-    npcFlipped.x -= 14;
-    npcFlipped.width = -npcRectangle.width; // Invert width to flip horizontally
-    return npcFlipped;
-  }
-};
-
-class Game {
-  vector<NPC> npcs;
-
-public:
-  void Create() {
-    // Adding values in npc obj;
-
-    STATE state = idle;
-    // npcs.push_back(NPC("assets/Villagers/3_Man", "Man", {0, 0, 48, 48}, {40,
-    // 40}, 2.5,state,-1,17,"Man"));
-
-    // till n no of npcs
-  }
-  void Update() {
-    for (int i = 0; i < npcs.size(); i++) {
-      STATE state = attack;
-      npcs[i].setState(state);
-    }
-  }
-  void Draw() {
-    for (int i = 0; i < npcs.size(); i++) {
-      STATE state = attack;
-      npcs[i].Draw();
-      npcs[i].setState(state);
-    }
-  }
-  void ProcessNPCs(vector<NPC> &npcs) {
-    for (int i = 0; i < npcs.size(); i++) {
-      //  npcs[i].Update();
-    }
-  }
-};
-int main() {
-  InitWindow(width, height, "My first RAYLIB program!");
   SetTargetFPS(60);
-  Game g;
-  // g.Create();
-  // g.Draw();
-  // Villager, Name,
 
-  NPC n("Woodcutter", {40, 40}, 2.5, STATE::idle, -1, 17, "MainCharacters");
-  // List of states
-  int stateC = 4;
-  STATE state_list[] = {idle, walk, attack, hurt, death};
+  Map map = LoadTiled("../assets/TileMap/Map.json"); // Load the map
+  //--------------------------------------------------------------------------------------
+
+  // Main game loop
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(BLACK);
-    // g.Update();
-    if (IsKeyPressed(KEY_SPACE)) {
-      n.setState(state_list[(++stateC) % 5]);
+    // Update
+    //--------------------------------------------------------------------------------------
+
+    // Move around with the keyboard
+    if (IsKeyDown(KEY_LEFT)) {
+      position.x += 2;
     }
-    n.Update();
+    if (IsKeyDown(KEY_UP)) {
+      position.y += 2;
+    }
+    if (IsKeyDown(KEY_RIGHT)) {
+      position.x -= 2;
+    }
+    if (IsKeyDown(KEY_DOWN)) {
+      position.y -= 2;
+    }
+
+    // Move based on mouse left click
+    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+      Vector2 delta = GetMouseDelta();
+      position.x += delta.x;
+      position.y += delta.y;
+    }
+
+    // Keep the map within screen bounds
+    if (position.x > 0) {
+      position.x = 0;
+    }
+    if (position.y > 0) {
+      position.y = 0;
+    }
+    if (position.x - GetScreenWidth() < -map.width * map.tileWidth) {
+      position.x = -map.width * map.tileWidth + GetScreenWidth();
+    }
+    if (position.y - GetScreenHeight() < -map.height * map.tileHeight) {
+      position.y = -map.height * map.tileHeight + GetScreenHeight();
+    }
+
+    // Draw
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+    {
+      ClearBackground(RAYWHITE);
+      DrawTiled(map, position.x, position.y, WHITE);
+    }
     EndDrawing();
+    //----------------------------------------------------------------------------------
   }
+
+  // De-Initialization
+  //--------------------------------------------------------------------------------------
+  UnloadMap(map);
 
   CloseWindow();
+  //--------------------------------------------------------------------------------------
+
   return 0;
 }
