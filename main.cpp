bf3b74e98f9a8f54ed1b32d386b7fdd07493f55f
ZunairaAli18 <143225771+ZunairaAli18@users.noreply.github.com>
2024-04-23 19:30:27 +0500

    npc_interactions constructor

--- main.cpp
+++ main.cpp
@@ -1,382 +1,297 @@
-#include <iostream>
 #include "math.h"
+#include "headers/ds.h"
+#include <bits/stdc++.h>
+#include "headers/UI.h"
+#include <iostream>
+#include <raylib.h>
 #include <string>
 #include <vector>
-#include <raylib.h>
 using namespace std;
 const int width = 800;
 const int height = 600;
-enum STATE{
-    attack,
-    walk,
-    idle,
-    hurt
-};
-class NPC_Walk{
-    Vector2 position;
-    public:
-    NPC_Walk(){}
-    void Random_walk(Vector2& npcPos)
-    {
-        position=npcPos;
-       int direction=GetRandomValue(0,3);
+enum STATE { attack, walk, idle, hurt, death };
 
-       switch(direction){
-        case 0:
-          position.y-=1;//Upward
-          break;
-        case 1:
-          position.y+=1;//Downward
-          break;
-        case 2:
-          position.x-=1;//Leftward
-          break;
-        case 3:
-          position.x+=1;
-          break;
-        default:
-          break;        
-       }
-    }
-    Vector2 getPosition(){
-        return position;
-    }
+class NPC_Characteristics {
+  STATE state;
+  int sentiment;
+  int age;
+  string occupation;
+  string name;
+
+public:
+  NPC_Characteristics(STATE state, int sentiment, int age, string occupation,
+                      string name) {
+    this->state = idle;
+    this->sentiment = 0;
+    this->age = age;
+    this->occupation = occupation;
+    this->name = name;
+  }
+  friend class NPC;
+  virtual void setState(STATE s) { this->state = s; }
+  int getSentiment() { return sentiment; }
+  int getAge() { return age; }
+  string getOccupation() { return occupation; }
+  string getName() { return name; }
+  STATE getState() { return state; }
+  void setName(string name) { this->name = name; }
+  void setAge(int age) { this->age = age; }
+  void setOccupation(string occupation) { this->occupation = occupation; }
+  void setSentiment(int sentiment) { this->sentiment = sentiment; }
 };
-class NPC{
-    NPC_Walk npc_walk;
-    STATE state;
-    int sentiment;
-    int age;
-    string occupation;
-    string texturePath;
-    string name;
-  Texture2D npcTexture;
-  Rectangle npcRectangle;
+
+class NPC_Physics {
   Vector2 npcPosition;
   bool colliding = false;
   float direction = 0.0f;
-  bool isMoving = false;
+  Rectangle npcRectangle;
   int flip = 0;
-  int frameSpeed = 6;
-  int frameCount = 0;
-  int currentFrame = 0;
   float npcSpeed;
 
 public:
-  NPC(string texPath, string _name,Rectangle rec, Vector2 pos, float spd,STATE state,int sentiment,int age,string occupation) {
-    npcRectangle = rec;
-    npcPosition = pos;
+  void Random_walk() {
+    direction +=
+        (rand() % 100 - 50) * 0.01f; // Adjust the range and step size as needed
+
+    // Calculate movement vector based on direction
+    Vector2 movement = {npcSpeed * cos(direction), npcSpeed * sin(direction)};
+
+    // Update NPC position
+    npcPosition.x += movement.x;
+    npcPosition.y += movement.y;
+    if (movement.x < 0.0f) {
+      flip = 1;
+    } else {
+      flip = 0;
+    }
+    // Ensure NPC stays within the screen boundaries
+    npcPosition.x = fmax(fmin(npcPosition.x, width - npcRectangle.width), 0);
+    npcPosition.y = fmax(fmin(npcPosition.y, height - npcRectangle.height), 0);
+  }
+  friend class NPC;
+};
+
+class NPC : public NPC_Physics, NPC_Characteristics {
+
+  float timer = 0.0f;
+  int frame = 0;
+  float frameWidth = 0;
+
+  bool isMoving = false;
+
+  string texturePath;
+  Texture2D npcTexture;
+  int frameSpeed = 6;
+  int frameCount = 0;
+  int currentFrame = 0;
+
+public:
+  NPC(string _name, Vector2 pos, float spd, STATE state, int sentiment, int age,
+      string occupation)
+      : NPC_Characteristics(state, sentiment, age, occupation, _name) {
     npcSpeed = spd;
-    texturePath = texPath;
-    name=_name;
-    npcTexture = LoadTexture((texturePath + "/" + name + ".png").c_str());
-    this->state=state;
-    this->sentiment=sentiment;
-    this->age=age;
-    this->occupation=occupation;
+    texturePath = "../assets/" + occupation + "/" + _name + "/" + _name;
+    setState(state);
+    npcPosition = pos;
+    npcRectangle = {0, 0, (float)npcTexture.height, (float)npcTexture.height};
   }
-  
-  void setState(STATE s)
-  {
-     state=s;
-     UnloadTexture(npcTexture);
-     switch (s)
-     {
-     case attack:
-        npcTexture = LoadTexture((texturePath + "/" + name + "_attack.png").c_str());
-        Animate(s);
-        break;
-     case walk:
-        npcTexture = LoadTexture((texturePath + "/" + name + "_walk.png").c_str());
-        npc_walk.Random_walk(npcPosition); // Update position
-        npcPosition = npc_walk.getPosition(); 
-        Animate(s);
-        break;
-     case idle:
-        npcTexture = LoadTexture((texturePath + "/" + name + ".png").c_str());
-        Animate(s);
-        break;
-     case hurt:
-        npcTexture = LoadTexture((texturePath + "/" + name + "_hurt.png").c_str());  
-        Animate(s);    
-        break;
-     default:
-        break;
-     }
-     
+
+  void setState(STATE s) {
+    state = s;
+    UnloadTexture(npcTexture);
+    switch (s) {
+    case walk:
+      npcTexture = LoadTexture((texturePath + "_walk.png").c_str());
+      break;
+    case idle:
+      npcTexture = LoadTexture((texturePath + "_idle.png").c_str());
+      break;
+    // One time
+    case attack:
+      npcTexture = LoadTexture((texturePath + "_attack.png").c_str());
+      break;
+    case hurt:
+      npcTexture = LoadTexture((texturePath + "_hurt.png").c_str());
+      break;
+    case death:
+      cout << " I CALL ONTO THE DEATH REAPER " << endl;
+      npcTexture = LoadTexture((texturePath + "_death.png").c_str());
+      break;
+    default:
+      break;
+    }
+    if (state == walk)
+      isMoving = true;
+    else
+      isMoving = false;
+    npcRectangle = {0, 0, (float)npcTexture.height, (float)npcTexture.height};
+    frame = 0;
   }
-  void Draw()
-  {
+  void Draw() {
     if (flip) {
-      Rectangle npcFlipped = npcRectangle;
-      npcFlipped.x =
-          npcRectangle.x + npcRectangle.width; // Set x-coordinate to right edge
-      npcFlipped.x -= 20;
-      npcFlipped.width =
-          -npcRectangle.width; // Invert width to flip horizontally
-      DrawTextureRec(npcTexture, npcFlipped, npcPosition, WHITE);
+      DrawTextureRec(npcTexture, flippedTexture(), npcPosition, WHITE);
     } else {
       DrawTextureRec(npcTexture, npcRectangle, npcPosition, WHITE);
     }
   }
-void Animate(STATE s)
-{
-    static float timer = 0.0f; 
-    static int frame = 0;      
-    float frameWidth = 0;
-    int count=0;
-    int numTextures=0;
-    if (npcTexture.width > 0 && npcTexture.height > 0)
-    {
-        numTextures = npcTexture.width / npcTexture.height;
-        if (numTextures > 0)
-        {
-            frameWidth = static_cast<float>(npcTexture.width / numTextures);
-        }
+  void Update() {
+    Animate();
+    if (isMoving)
+      Random_walk();
+    Draw();
+  }
+  void Animate() {
+    int count = 0;
+    int numTextures = 0;
+    if (npcTexture.width > 0 && npcTexture.height > 0) {
+      numTextures = npcTexture.width / npcTexture.height;
+      if (numTextures > 0) {
+        frameWidth = static_cast<float>(npcTexture.width / numTextures);
+      }
     }
     int maxFrames = static_cast<int>(npcTexture.width / frameWidth);
-   if(s==walk){
-        timer += GetFrameTime();
-    if (timer >= 0.3f)
-    {
-        timer = 0.0f;
-        frame += 1;
+    timer += GetFrameTime();
+    if (timer >= 0.3f) {
+      timer = 0.0f;
+      frame += 1;
+      if (state == walk || state == idle)
+        frame = frame % maxFrames;
+      else {
+        if (frame >= maxFrames)
+          frame = maxFrames - 1;
+      }
     }
-    frame = frame % maxFrames;
-    DrawTextureRec(
-        npcTexture,
-        Rectangle{frameWidth * frame, 0, frameWidth, static_cast<float>(npcTexture.height)},
-        Vector2{20, 20},
-        RAYWHITE
-    );
 
-   }
-   else{
-     timer += GetFrameTime();
-    if (timer >= 0.3f  && frame<=numTextures-2)
-    {
-        timer = 0.0f;
-        frame += 1;
-        count++;
-    }
-    DrawTextureRec(
-        npcTexture,
-        Rectangle{frameWidth * frame, 0, frameWidth, static_cast<float>(npcTexture.height)},
-        Vector2{20, 20},
-        RAYWHITE
-    );
-
-   }
-   
-}
-
-
-  
-
-};
-
-/*class NPC{
-    Actions action;
-    string name;
-    int age;
-    int sentiment_val;
-    int gender;//1=male 0=female
-    Occupation occp;
-    string texturePath;
-    Texture2D npcTexture;
-    Rectangle npcRectangle;
-    Vector2 npcPosition;
-    float npcSpeed;
-    float direction = 0.0f;
-  bool isMoving = false;
-  int flip = 0;
-  int frameSpeed = 6;
-  int frameCount = 0;
-  int currentFrame = 0;
-  
-    public:
-    NPC(string texPath, Rectangle rec, Vector2 pos, float spd,string name,int age,int sent_val,int gender,Occupation &o,Actions act):name(name),age(age),sentiment_val(sent_val),gender(gender),occp(o),action(act){
-    npcRectangle = rec;
-    npcPosition = pos;
-    npcSpeed = spd;
-    npcTexture = LoadTexture(texPath.c_str());
-    texturePath = texPath;
-    
+    cout << frame << endl;
+    npcRectangle = {frameWidth * frame, 0, frameWidth,
+                    static_cast<float>(npcTexture.height)};
   }
-  //draw
-  void Draw() {
-    float frame_width=(float)npcTexture.width/4;
-    float timer=0.0f;
-    int frame=0;
-    int maxframe=(npcTexture.width/frame_width);
-    timer+=GetFrameTime();
-    if(timer>=0.2f)
-    {
-        timer=0.0f;
-        frame+=1;
-    }
-    frame=frame%maxframe;
-     DrawTextureRec(npcTexture,Rectangle{frame_width*frame,0,frame_width,(float)npcTexture.height},Vector2{20,20},RAYWHITE);
-   }  
-   //random pos update
-   void update_pos()
-   {
-    isMoving = GetRandomValue(0, 100) > 25;
-    if (isMoving) {
-      direction += (rand() % 100 - 50) *
-                   0.01f; // Adjust the range and step size as needed
-
-      // Calculate movement vector based on direction
-      Vector2 movement = {npcSpeed * cos(direction), npcSpeed * sin(direction)};
-
-      // Update NPC position
-      npcPosition.x += movement.x;
-      npcPosition.y += movement.y;
-      if (movement.x < 0.0f) {
-        flip = 1;
-      } else {
-        flip = 0;
-      }
-      // Ensure NPC stays within the screen boundaries
-      npcPosition.x = fmax(fmin(npcPosition.x, width - npcRectangle.width), 0);
-      npcPosition.y =
-          fmax(fmin(npcPosition.y, height - npcRectangle.height), 0);
-      frameCount++;
-      if (frameCount >= (60 / frameSpeed)) {
-        frameCount = 0;
-        currentFrame++;
-        currentFrame = currentFrame % 5;
-        npcRectangle.x = currentFrame * 48;
-      }
-    }
-   }
-   //actions
-   void random_move(){
-      update_pos();
-   } 
-   void animate_walk()
-   {
-      UnloadTexture(npcTexture);
-      npcTexture=LoadTexture("assets/Villagers/3_Man/Man_walk.png");
-   }
-   void animate_slap()
-   {
-      UnloadTexture(npcTexture);
-      npcTexture=LoadTexture("");
-   }
-   void animate_run(){
-      UnloadTexture(npcTexture);
-      npcTexture=LoadTexture("");
-   }
-   void animate_attack()
-   {
-       UnloadTexture(npcTexture);
-       npcTexture=LoadTexture("");
-   }
-   void animate_die()
-   {
-       UnloadTexture(npcTexture);
-       npcTexture=LoadTexture("");
-   }
-   //.....furtheractions
-   //getter setter
-   void setAction(Actions act) {
-        action = act;
-    }
-
-    Actions getAction() {
-        return action;
-    }
-   //update texture
-   void Update() {
-        switch(action) {
-            case WALK:
-            
-                animate_walk();
-                random_move();
-                break;
-            case RUN:
-                animate_run();
-                random_move();
-                break;
-            case SLAP:
-                animate_slap();  
-                break;
-             case ATTACK:
-                animate_attack();
-                break;
-            case DIE:
-                animate_die();
-                break;    
-            default:
-                random_move();
-                break;
-        }
-    }
-    //after actions like slap or attack 
-    //will activate for the other npc
-    void change_color()
-    {
-        Color tint={255, 150, 150, 255};
-        DrawTextureV(npcTexture, npcPosition,tint);
-    }
-    
-};*/
-class Game{
-    vector<NPC> npcs;
-   public:
-    void Create()
-    {
-       //Adding values in npc obj;
-       
-       STATE state=idle;
-   // npcs.push_back(NPC("assets/Villagers/3_Man", "Man", {0, 0, 48, 48}, {40, 40}, 2.5,state,-1,17,"Man"));
-    
-    //till n no of npcs
-    }
-    void Update()
-    {
-         for(int i=0;i<npcs.size();i++)
-         {
-          STATE state=attack;
-           npcs[i].setState(state);
-         }
-    }
-    void draw()
-    {
-        for(int i=0;i<npcs.size();i++)
-        {
-            STATE state=attack;
-            npcs[i].Draw();
-            npcs[i].setState(state);
-        }
-    }
-   void ProcessNPCs(vector<NPC> &npcs){
-       for(int i=0;i<npcs.size();i++)
-       {
-      //  npcs[i].Update();
-       }
-   }
+  Rectangle GetRect() { return npcRectangle; }
+  Vector2 GetPos() { return npcPosition; }
+private:
+  Rectangle flippedTexture() {
+    Rectangle npcFlipped = npcRectangle;
+    npcFlipped.x =
+        npcRectangle.x + npcRectangle.width; // Set x-coordinate to right edge
+    npcFlipped.x -= 14;
+    npcFlipped.width = -npcRectangle.width; // Invert width to flip horizontally
+    return npcFlipped;
+  }
 };
-int main()
-{
-    InitWindow(width, height, "My first RAYLIB program!");
-    SetTargetFPS(60);
-    Game g;
-    //g.Create();
-    //g.draw();
-    STATE state=idle;
-    NPC n("assets/Villagers/3_Man", "Man", {0, 0, 48, 48}, {40, 40}, 2.5,state,-1,17,"Man");
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(BLACK);
-       // g.Update();
-       STATE state=walk;
-       n.setState(state);
-        EndDrawing();
-    }
 
-    CloseWindow();
-    return 0;
+class NPC_Interactions {
+  vector<NPC *> npcList;
+  QuadTree<NPC> *Q;
+
+public:
+  NPC_Interactions(){
+    int attackers_num=10;
+    int healers_num=30;
+    int villagers_num=70;
+    for(int i=0;i<10;i++){
+      npcList.push_back(new NPC("Snuffy", {40, 40}, 2.5, STATE::idle, -1, 17, "Greenzone"));
+    }
+    for(int i=0;i<10;i++){
+      npcList.push_back(new NPC("Huffy", {40, 40}, 2.5, STATE::idle, -1, 17, "Greenzone"));
+    }
+    for(int i=0;i<10;i++){
+      npcList.push_back(new NPC("Bob", {40, 40}, 2.5, STATE::idle, -1, 17, "Greenzone"));
+    }
+    for(int i=0;i<3;i++){
+      npcList.push_back(new NPC("Woodcutter", {40, 40}, 2.5, STATE::idle, -1, 17, "MainCharacters"));
+    }
+    for(int i=0;i<3;i++){
+      npcList.push_back(new NPC("SteamMan", {40, 40}, 2.5, STATE::idle, -1, 17, "MainCharacters"));
+    }
+    for(int i=0;i<4;i++){
+      npcList.push_back(new NPC("GraveRobber", {40, 40}, 2.5, STATE::idle, -1, 17, "MainCharacters"));
+    }
+    for(int i=0;i<13;i++){
+      npcList.push_back(new NPC("Man", {40, 40}, 2.5, STATE::idle, -1, 17, "Villagers"));
+    }
+    for(int i=0;i<13;i++){
+      npcList.push_back(new NPC("Woman", {40, 40}, 2.5, STATE::idle, -1, 17, "Villagers"));
+    }
+    for(int i=0;i<10;i++){
+      npcList.push_back(new NPC("Old_man", {40, 40}, 2.5, STATE::idle, -1, 17, "Villagers"));
+    }
+    for(int i=0;i<10;i++){
+      npcList.push_back(new NPC("Old_woman", {40, 40}, 2.5, STATE::idle, -1, 17, "Villagers"));
+    }
+    for(int i=0;i<12;i++){
+      npcList.push_back(new NPC("Girl", {40, 40}, 2.5, STATE::idle, -1, 17, "Villagers"));
+    }
+    for(int i=0;i<12;i++){
+      npcList.push_back(new NPC("Boy", {40, 40}, 2.5, STATE::idle, -1, 17, "Villagers"));
+    }
+  }
+  bool run = true;
+  void AddNPC(NPC *npc) { npcList.push_back(npc); }
+
+  void CheckCollisions() {
+    for (NPC *npc : npcList) {
+      bool flag = false;
+      Circle c = {npc->GetPos(), 60};
+      c.center.x += npc->GetRect().width / 2 - 10;
+      c.center.y += npc->GetRect().height / 2;
+      // DrawCircleLinesV(c.center, c.radius, RED);
+      vector<Point<NPC>> collided = Q->query(c);
+      // cout << npc->GetName() << " " << collided.size() << endl;
+      for (Point other : collided) {
+        if (other.data != npc) {
+          flag = true;
+          // npc->setColliding(true);
+          // other.data->setColliding(true);
+        }
+      }
+
+      if (!flag) {
+        // npc->setColliding(false);
+      }
+    }
+    delete Q;
+  }
+  void Draw() {
+    Q = new QuadTree<NPC>(Rectangle{0, 0, width, height}, 4);
+    for (NPC *npc : npcList) {
+      npc->Draw();
+      Point<NPC> p = {npc->GetPos(), npc};
+      Q->insert(p);
+      // cout << npc->GetName() << " " << p.v.x << " " << p.v.y << endl;
+    }
+    // Q->Draw();
+  }
+  void Update() {
+    if (run) {
+      for (NPC *npc : npcList) {
+        npc->Update();
+      }
+    }
+  }
+};
+
+int main() {
+  InitWindow(width, height, "My first RAYLIB program!");
+  SetTargetFPS(60);
+  // g.Create();
+  // g.Draw();
+  // Villager, Name,
+
+  NPC n("Woodcutter", {40, 40}, 2.5, STATE::idle, -1, 17, "MainCharacters");
+  // List of states
+  int stateC = 4;
+  STATE state_list[] = {idle, walk, attack, hurt, death};
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(BLACK);
+    // g.Update();
+    if (IsKeyPressed(KEY_SPACE)) {
+      n.setState(state_list[(++stateC) % 5]);
+    }
+    n.Update();
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
